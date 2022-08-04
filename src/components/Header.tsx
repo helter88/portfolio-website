0a9c8aa4b8ad062df45ebed1efc370c58c9bc6ed
Artur <helter@wp.pl>
2022-08-04 18:45:18 +0200

    add text and animation to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,13 @@ import styles from './Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      
+        <div className={styles['text-box']}>
+            <h1 className={styles['heading-primary']}>
+            <span className={styles['heading-primary-main']}>ARTUR HELTA</span>
+            <span className={styles['heading-primary-sub']}>React & Typescript Developer</span>
+        </h1>
+        </div>
+        
     </header>
   )
 }
