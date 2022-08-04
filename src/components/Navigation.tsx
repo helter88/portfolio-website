ac20c68e9e3f6832b3b6cd7c1cccde95512a7a6b
Artur <helter@wp.pl>
2022-08-04 17:02:38 +0200

    add navigation text and position

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,19 @@ import styles from './Navigation.module.css';
 const Navigation = () => {
   return (
     <div className={styles.navigation}>
-      <img src={logo} alt='logo' className={styles.logo} />
+        <div className={styles['image-container']}>
+          <img src={logo} alt='logo' className={styles.logo} />  
+        </div>
+      
+      <div className={styles['selection-nav-container']}>
+        <ul>
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+        <li>Contact</li>
+        </ul>
+        
+      </div>
     </div>
   )
 }
