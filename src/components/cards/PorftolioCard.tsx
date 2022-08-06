298b2a5a6603818ccdea39176be7cd21b926ec39
Artur <helter@wp.pl>
2022-08-06 20:10:54 +0200

    add icon and description to PortfolioCard

--- src/components/cards/PorftolioCard.tsx
+++ src/components/cards/PorftolioCard.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import styles from './PortfolioCard.module.css';
+import { ReactComponent as Globe } from './../../img/card-icons/internet-icon.svg';
 
 const PortfolioCard = () => {
   return (
     <div className={styles.card}>
       <div className={styles['logo-box']}>
-        Logo
+        <div className={styles['logo-dimentions']}>
+          <Globe fill="#D3F4FF" width='100%'/>
+        </div>
+        
       </div>
-      <div className={styles['title-card']}>
-        title
-      </div>
+        <h3 className={styles['title']}>Portfolio website</h3>
+        <h6>It is current webside made to show programming skills in specific technologiest.</h6>
+        <div className={styles['tech-list-box']}>
+          <p>React</p>
+          <p>Typescript</p>
+          <p>CSS Modules Stylesheet</p>
+        </div>
+        
       
     </div>
   )
