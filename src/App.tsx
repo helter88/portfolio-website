af4f8c84e5ba7b1a16b9ebcd023856330a1fa1f3
Artur <helter@wp.pl>
2022-08-06 16:36:40 +0200

    add Portfolio component and title with gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/Hero';
 import Header from './components/Header';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero/>
+      <Portfolio />
       
     </div>
   );
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Portfolio.module.css'
+
+const Portfolio = () => {
+  return (
+    <section className={styles.portfolio}>
+        <div className={styles['text-box']}><h2 className={styles['heading-secondary']}>PORTFOLIO</h2></div>
+      
+    </section>
+  )
+}
+
+export default Portfolio
