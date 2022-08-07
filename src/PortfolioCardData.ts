941acfc3c53cedbf27ea2a969d508647f5048903
Artur <helter@wp.pl>
2022-08-07 20:28:11 +0200

    create PortfoliCardData file

---
+++ src/PortfolioCardData.ts
@@ -0,0 +1,48 @@
+
+export const PortfolioCardData = [
+    {
+        title: 'Portfolio Website',
+        logo: 'Globe',
+        description: 'This website was made to present frontend skills by using modern framework, animations and own design.',
+        tools: ['React', 'Typescript','CSS Modules Stylesheet' ]
+
+    },
+    {
+        title: 'Send invoices to your client',
+        logo: 'Invoice',
+        description: 'The aplication was made to help sending a lot of invoices to group of clients. You should add all your invoices and list of clients in order to send them e-mail with invoice.',
+        tools: ['React', 'Typescript','Talwind' ]
+
+    },
+   {
+       title: 'Insert blood pressure data to excel file',
+    logo: 'BloodPressure',
+    description: 'The aplication was made to help insert data to excel file from data colection which was written in notepad.',
+    tools: ['React', 'Typescript','Talwind' ] 
+    },{
+        title: 'Register Form',
+     logo: 'RegisterForm',
+     description: 'Write your e-mail and password in proper form, click button and send your data. Aplication was made to train custom hooks for validation logic in React',
+     tools: ['React', 'Typescript','Talwind' ] 
+     },{
+        title: 'ToDoList with Redux Toolkit',
+        logo: 'ToDoList',
+        description: 'Make list with tasks and dalete them if done. It was made to train Redux Toolkit skills in React Tyoescript',
+        tools: ['React', 'Typescript', 'Redux Toolkit','Talwind' ] 
+     },
+     {
+        title: 'ToDoList2 with Redux',
+        logo: 'ToDoList',
+        description: 'Make list with tasks and dalete them if done. It was made to train Redux skills in React Tyoescript',
+        tools: ['React', 'Typescript', 'Redux','Talwind' ] 
+     },
+     {
+        title: 'Get data from API with Redux Toolkit',
+        logo: 'ToDoList',
+        description: 'This project was made to train getting, storing and displaying data from simple API using Redux Toolkit ',
+        tools: ['React', 'Typescript', 'Redux Toolkit','Talwind' ] 
+     },
+
+    
+   
+]
