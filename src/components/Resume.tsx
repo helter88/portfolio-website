e9ad96ea1da36435632ebf08d24d7465443b2fcb
Artur <helter@wp.pl>
2022-08-10 13:47:21 +0200

    change NavigationButton for more reusable

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,6 +5,7 @@ const Resume = () => {
     <section className={styles.resume}>
       <div className={styles['text-box']}>
       <h2 className={styles['heading-secondary']}>RESUME</h2>
+      <p className={styles.description}>In order to get more information about me:</p>
       </div>
     </section>
   )
