a473ca51f4d4ae2cf8a64ab8db523d6339e04d43
Artur <helter@wp.pl>
2022-08-11 19:45:00 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Skills />
       <Resume />
       <Contact />
-      
+      <Footer />
     </div>
   );
 }
