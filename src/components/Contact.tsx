70b47bf3c4212171967a40b56b4d07146c8f4a84
Artur <helter@wp.pl>
2022-08-11 14:00:05 +0200

    create ContactCard component ad fill column in ContactCard component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './Contact.module.css';
+import ContactCard from './ContactCard';
 
 const Contact = () => {
   return (
     <section className={styles.contact}>
       <div className={styles['text-box']}><h2 className={styles['heading-secondary']}>CONTACT</h2></div>
       <div className={styles['contact-container']}>
-        <div className={styles['first-column']}>Fist column</div>
+        <div className={styles['first-column']}>
+          <ContactCard title='Phone' description='+48 729 614 129'/>
+          <ContactCard title='Email' description='arturhelta@onet.pl'/>
+          <ContactCard title='LinkedIn' description='Artur Helta'/>
+          <ContactCard title='Github' description='helter88'/>
+        
+
+        </div>
         <div className={styles['second-column']}>Second column</div>
       </div>
     </section>
---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './ContactCard.module.css';
+import {ReactComponent as Phone} from './../img/contact-icons/mail-icon.svg'
+
+interface CardType{
+    title: string;
+    description: string
+}
+
+const ContactCard: React.FC<CardType> = ({title, description}) => {
+  return (
+    
+    <div className={styles['contact-box']}>
+        <div className={styles['contact-icon-box']}>
+            <Phone fill= '#D3F4FF'/>
+        </div>
+        <div className={styles['contact-text-box']}>
+            <h6>{title}</h6>
+            <p>{description}</p>
+        </div>
+        
+    </div>
+            
+  )
+}
+
+export default ContactCard
