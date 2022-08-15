daec0e7eef2ddafa3493005150446418868ed515
Artur <helter@wp.pl>
2022-08-15 15:44:11 +0200

    implement react pdf viewer

--- src/components/DisplayCV.tsx
+++ src/components/DisplayCV.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const DisplayCV = () => {
   return (
-    <div>
-      <h3>CV</h3>
-    </div>
+    <>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
+        <Viewer fileUrl='./../example.pdf'/>;
+       </Worker>
+    </>
   )
 }
 
