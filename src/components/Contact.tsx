4dc5d2d1be73aca3bab1b9bd121f8650de5a64c8
Artur <helter@wp.pl>
2022-09-14 19:14:43 +0200

    adapt icons in Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,16 +3,17 @@ import styles from './Contact.module.css';
 import ContactCard from './ContactCard';
 import ContactForm from './ContactForm';
 
+
 const Contact = () => {
   return (
     <section className={styles.contact} id='contact'>
       <div className={styles['text-box']}><h2 className={styles['heading-secondary']}>CONTACT</h2></div>
       <div className={styles['contact-container']}>
         <div className={styles['first-column']}>
-          <ContactCard title='Phone' description='+48 729 614 129'/>
-          <ContactCard title='Email' description='arturhelta@onet.pl'/>
-          <ContactCard title='LinkedIn' description='Artur Helta'/>
-          <ContactCard title='Github' description='helter88'/>
+          <ContactCard title='Phone' description='+48 729 614 129' icon='Phone'/>
+          <ContactCard title='Email' description='arturhelta@onet.pl' icon='Email'/>
+          <ContactCard title='LinkedIn' description='Artur Helta' icon='LinkedIn'/>
+          <ContactCard title='Github' description='helter88' icon='Github'/>
 
         </div>
         <div className={styles['second-column']}>
--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import styles from './ContactCard.module.css';
-import {ReactComponent as Phone} from './../img/contact-icons/mail-icon.svg'
+import {ReactComponent as Email} from './../img/contact-icons/mail-icon.svg'
+import {ReactComponent as LinkedIn} from './../img/contact-icons/linkedin-icon.svg'
+import {ReactComponent as Phone} from './../img/contact-icons/phone-icon.svg'
+import {ReactComponent as Github} from './../img/contact-icons/github-icon.svg'
 
+interface IconsType {
+  Email: typeof Email,
+  LinkedIn: typeof LinkedIn,
+  Phone: typeof Phone,
+  Github: typeof Github
+}
 interface CardType{
-    title: string;
-    description: string
+  title: string;
+  description: string;
+  icon: keyof IconsType;
 }
 
-const ContactCard: React.FC<CardType> = ({title, description}) => {
+const iconContainer: IconsType = {
+  Email,
+  LinkedIn,
+  Phone,
+  Github
+
+}
+
+
+const ContactCard: React.FC<CardType> = ({title, description, icon}) => {
+  const Icon = iconContainer[icon];
   return (
     
     <div className={styles['contact-box']}>
         <div className={styles['contact-icon-box']}>
-            <Phone fill= '#007F81'/>
+            <Icon fill='#007F81' width="100%" height="100%"/>
         </div>
         <div className={styles['contact-text-box']}>
             <h6>{title}</h6>
