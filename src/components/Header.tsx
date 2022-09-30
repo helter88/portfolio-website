91571059528a0bd93e4c6ff50c48a54178ef7128
Artur <helter@wp.pl>
2022-09-30 19:02:26 +0200

    implement some responsivness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../img/Logo-AH.png';
 import styles from './Header.module.css';
 import {Link} from 'react-scroll';
+import { ReactComponent as Hamburger } from './../img/hamburger-icon.svg';
 
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  console.log(isClicked);
+
 
   const toogleHome= () =>{
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
@@ -15,7 +20,7 @@ const Header = () => {
           <img src={logo} alt='logo' className={styles.logo} onClick={toogleHome} />  
         </div>
       
-      <div className={styles['nav-container']}>
+      <div className={styles[`nav-container ${isClicked && 'active'}`]}>
         <ul>
         <li><Link activeClass={styles.active} to="hero" spy={true} smooth={true} className={styles['top-nav-bar']}>Home</Link></li>
         <li><Link activeClass={styles.active} to="portfolio" spy={true} smooth={true} className={styles['top-nav-bar']}>Portfolio</Link></li>
@@ -25,6 +30,7 @@ const Header = () => {
         </ul>
         
       </div>
+      <div className={styles['hamburger-container']} onClick={()=>setIsClicked(!isClicked)} ><Hamburger /></div>
     </div>
   )
 }
