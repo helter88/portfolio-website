5c3071f1856f24ebb24e5ffc6b0d0de815f9b2fa
Artur <helter@wp.pl>
2022-10-27 12:14:55 +0200

    adapt responsive style to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,9 @@ import {Link} from 'react-scroll';
 import { ReactComponent as Hamburger } from './../img/hamburger-icon.svg';
 
 const Header = () => {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  console.log(isClicked);
-
+  const toggleOpen = () => setIsOpen(!isOpen);
 
   const toogleHome= () =>{
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
@@ -20,17 +19,22 @@ const Header = () => {
           <img src={logo} alt='logo' className={styles.logo} onClick={toogleHome} />  
         </div>
       
-      <div className={styles[`nav-container ${isClicked && 'active'}`]}>
+      <div className={`${styles['nav-container']} ${isOpen && styles.active}`}>
         <ul>
-        <li><Link activeClass={styles.active} to="hero" spy={true} smooth={true} className={styles['top-nav-bar']}>Home</Link></li>
-        <li><Link activeClass={styles.active} to="portfolio" spy={true} smooth={true} className={styles['top-nav-bar']}>Portfolio</Link></li>
-        <li><Link activeClass={styles.active} to="skills" spy={true} smooth={true} className={styles['top-nav-bar']}>Skills</Link></li>
-        <li><Link activeClass={styles.active} to="resume" spy={true} smooth={true} className={styles['top-nav-bar']}>Resume</Link></li>
-        <li><Link activeClass={styles.active} to="contact" spy={true} smooth={true} className={styles['top-nav-bar']}>Contact</Link></li>
+        <li><Link activeClass={styles.active} to="hero" spy={true} 
+        smooth={true} className={styles['top-nav-bar']} onClick={toggleOpen}>Home</Link></li>
+        <li><Link activeClass={styles.active} to="portfolio" spy={true}
+         smooth={true} className={styles['top-nav-bar']} onClick={toggleOpen}>Portfolio</Link></li>
+        <li><Link activeClass={styles.active} to="skills" spy={true}
+         smooth={true} className={styles['top-nav-bar']} onClick={toggleOpen}>Skills</Link></li>
+        <li><Link activeClass={styles.active} to="resume" spy={true}
+         smooth={true} className={styles['top-nav-bar']} onClick={toggleOpen}>Resume</Link></li>
+        <li><Link activeClass={styles.active} to="contact" spy={true}
+         smooth={true} className={styles['top-nav-bar']} onClick={toggleOpen}>Contact</Link></li>
         </ul>
         
       </div>
-      <div className={styles['hamburger-container']} onClick={()=>setIsClicked(!isClicked)} ><Hamburger /></div>
+      <div className={styles['hamburger-container']} onClick={toggleOpen} ><Hamburger /></div>
     </div>
   )
 }
