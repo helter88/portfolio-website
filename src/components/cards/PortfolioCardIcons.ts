874a0fff73a6b97101e5c28b0a558baf1a643e32
Artur <helter@wp.pl>
2023-01-13 16:36:41 +0100

    add new project to portfolio

--- src/components/cards/PortfolioCardIcons.ts
+++ src/components/cards/PortfolioCardIcons.ts
@@ -1,23 +1,26 @@
-import { ReactComponent as Globe } from "./../../img/card-icons/internet-icon.svg";
-import { ReactComponent as RegisterForm } from "./../../img/card-icons/RegisterForm-icon.svg";
-import { ReactComponent as ToDoList } from "./../../img/card-icons/ToDoList-icon.svg";
-import { ReactComponent as GetData } from "./../../img/card-icons/GetData-icon.svg";
-import { ReactComponent as Shop } from "./../../img/card-icons/e-commerse-icon.svg";
+import { ReactComponent as Globe } from './../../img/card-icons/internet-icon.svg';
+import { ReactComponent as RegisterForm } from './../../img/card-icons/RegisterForm-icon.svg';
+import { ReactComponent as ToDoList } from './../../img/card-icons/ToDoList-icon.svg';
+import { ReactComponent as GetData } from './../../img/card-icons/GetData-icon.svg';
+import { ReactComponent as Shop } from './../../img/card-icons/e-commerse-icon.svg';
+import { ReactComponent as BloodPressure } from './../../img/card-icons/BloodPressure-icon.svg';
 
 export interface IconsType {
-  Globe: typeof Globe;
-  Shop: typeof Shop;
-  RegisterForm: typeof RegisterForm;
-  ToDoList: typeof ToDoList;
-  GetData: typeof GetData;
+	Globe: typeof Globe;
+	Shop: typeof Shop;
+	RegisterForm: typeof RegisterForm;
+	ToDoList: typeof ToDoList;
+	GetData: typeof GetData;
+	BloodPressure: typeof BloodPressure;
 }
 
 const icons: IconsType = {
-  Globe,
-  Shop,
-  RegisterForm,
-  ToDoList,
-  GetData,
+	Globe,
+	Shop,
+	RegisterForm,
+	ToDoList,
+	GetData,
+	BloodPressure,
 };
 
 export default icons;
